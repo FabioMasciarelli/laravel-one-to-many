e50b6a8f2e6dfbc9250db76f8664a931c0536c3d
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-06-24 17:26:07 +0200

    feat: print in page type for each project

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -8,8 +8,12 @@
 
     <h4>Ultima modifica: {{ $project->latest_fix }}</h4>
 
+    <h5>Tipo: {{ $project->type_id ? $project->type->name : 'Undefined' }}</h5>
+
     <h6>Readme: {{ $project->readme }}</h6>
 
     <h6>Linguaggio: {{ $project->languages }}</h6>
+
 </div>
+
 @endsection
